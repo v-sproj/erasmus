5be0a18d4376d0db24b54d012b4478298c8b5395
v-sproj <sprojcarova.val@seznam.cz>
2025-02-06 16:18:47 +0100

    už skoro hotovo

--- src/js/main.js
+++ src/js/main.js
@@ -39,7 +39,22 @@ document.querySelectorAll('.nav-link').forEach(link => {
   });
 });
 
+(() => {
+  'use strict'
 
+  const forms = document.querySelectorAll('.needs-validation')
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
 
 
 
